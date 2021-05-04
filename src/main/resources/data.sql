ecc4ac5e9d740717e31d7e0bab8209d390b121f9
Marcelo Lima <marcelo.souza@azi.com.br>
2021-05-04 15:59:40 -0400

    Created resources

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('JOAO', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('PEDRO', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('JOSE', 250.0);
