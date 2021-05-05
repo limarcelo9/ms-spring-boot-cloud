7bd710fcceee0824b9c05b8703d3292059de443a
unknown <limarcelo90@gmail.com>
2021-05-04 22:21:05 -0400

    hr-worker implementation

--- src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll(){
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Worker worker = repository.findById(id).get();
+        return ResponseEntity.ok(worker);
     }
 }
